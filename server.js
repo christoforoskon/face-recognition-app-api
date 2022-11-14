5e3d2f8e1f31f5356fef5be19af92688517cc3e1
christoforoskon <xristoforos.knks@yahoo.gr>
2022-11-14 23:40:52 +0200

    included CORS and register request fixed

--- server.js
+++ server.js
@@ -1,15 +1,18 @@
 const express = require("express");
 const bcrypt = require("bcrypt-nodejs");
+const cors = require("cors");
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 const database = {
     users: [{
             id: "123",
             name: "John",
             email: "john@gmail.com",
+            password: "cookies",
             entries: 0,
             joined: new Date(),
         },
@@ -17,6 +20,7 @@ const database = {
             id: "124",
             name: "Sally",
             email: "sally@gmail.com",
+            password: "bananas",
             entries: 0,
             joined: new Date(),
         },
@@ -53,7 +57,7 @@ app.post("/signin", (req, res) => {
         req.body.email === database.users[0].email &&
         req.body.password === database.users[0].password
     ) {
-        res.json("success");
+        res.json(database.users[0]);
     } else {
         res.status(400).json("error logging in");
     }
@@ -69,7 +73,6 @@ app.post("/register", (req, res) => {
         id: "125",
         name: name,
         email: email,
-        password: password,
         entries: 0,
         joined: new Date(),
     });
